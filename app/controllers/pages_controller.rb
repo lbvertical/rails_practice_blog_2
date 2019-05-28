59c03bf4100d3752c922903d100950cdc7f67f9b
Logan Bryant <loganbryantvertical@gmail.com>
2019-05-27 18:34:52 -0600

    updating the blog code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,6 +21,25 @@ class PagesController < ApplicationController
     end
   end
 
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+
 private
 
   def pages_params
